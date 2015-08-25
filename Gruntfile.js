a0e4c30cc26829133f6ccb5783e7f07c8436a697
tajoy <tj32811124@gmail.com>
2015-08-25 20:47:30 +0800

    ...

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
 var fs = require('fs');  
-  
+var account = 'tajoy@foxmail.com';
 function readSyn() {  
-   process.stdin.pause();  
+   process.stdin.pause();
+   console.log("account : " + account);
+   console.log("enter password: ");
    var response = fs.readSync(process.stdin.fd, 1000, 0, "utf8");  
    process.stdin.resume();  
-   return response[0].trim();  
+   return response[0].trim();
 }
 
 module.exports = function(grunt) {
@@ -13,7 +15,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         screeps: {
             options: {
-                email: 'tajoy@foxmail.com',
+                email: account ,
                 password: readSyn(),
                 branch: 'default'
             },
